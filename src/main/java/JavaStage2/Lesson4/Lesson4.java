300576fafd2d15794a6f242632fd58e58792d992
MikhailYurkov <9906205@gmail.com>
2022-07-17 16:46:06 +0300

    HW 2.4.1

--- src/main/java/JavaStage2/Lesson4/Lesson4.java
+++ src/main/java/JavaStage2/Lesson4/Lesson4.java
@@ -28,19 +28,15 @@ public class Lesson4 {
         System.arraycopy(arr, 0, arr1, 0, HALF_SIZE);
         System.arraycopy(arr, HALF_SIZE, arr2, 0, HALF_SIZE);
 
-        new Thread() {
-            public void run() {
-                float[] a1 = calculate(arr1);
-                System.arraycopy(a1, 0, arr1, 0, a1.length);
-            }
-        }.start();
+        new Thread(() -> {
+            float[] a1 = calculate(arr1);
+            System.arraycopy(a1, 0, arr1, 0, a1.length);
+        }).start();
 
-        new Thread() {
-            public void run() {
-                float[] a2 = calculate(arr2);
-                System.arraycopy(a2, 0, arr2, 0, a2.length);
-            }
-        }.start();
+        new Thread(() -> {
+            float[] a2 = calculate(arr2);
+            System.arraycopy(a2, 0, arr2, 0, a2.length);
+        }).start();
 
         System.arraycopy(arr1, 0, arr, 0, HALF_SIZE);
         System.arraycopy(arr2, 0, arr, HALF_SIZE, HALF_SIZE);
