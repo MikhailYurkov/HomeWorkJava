14b102a4d15e6f1b8348190626ec3856bdc7c997
MikhailYurkov <9906205@gmail.com>
2022-08-07 12:17:25 +0300

    HW 3.1

---
+++ src/main/java/JavaStage3/Lesson1/Fruit.java
@@ -0,0 +1,6 @@
+package JavaStage3.Lesson1;
+
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
---
+++ src/main/java/JavaStage3/Lesson1/Orange.java
@@ -0,0 +1,8 @@
+package JavaStage3.Lesson1;
+
+public class Orange extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.5f;
+    }
+}
