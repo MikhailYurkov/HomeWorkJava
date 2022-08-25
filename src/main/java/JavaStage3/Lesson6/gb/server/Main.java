2f4a343e723560608eac4477f3137755ab7ded73
MikhailYurkov <9906205@gmail.com>
2022-08-25 19:42:40 +0300

    HW 3.6

---
+++ src/main/java/JavaStage3/Lesson6/gb/server/Main.java
@@ -0,0 +1,10 @@
+package JavaStage3.Lesson6.gb.server;
+
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        new Server();
+    }
+}
